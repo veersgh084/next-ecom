463ddf75296e99c3d6cd1f71a30ce8592e37ba23
veersgh084 <veersgh084@gmail.com>
2025-01-16 15:11:38 +0530

    second changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <p className="text-center sm:text-left text-lg sm:text-xl">Hello sir</p>
+       this is my first page
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
