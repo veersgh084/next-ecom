9c2057620ee98b8bdfe0a786b97287c7a7c53ad8
veersgh084 <veersgh084@gmail.com>
2025-01-16 14:47:15 +0530

    Initial commit

---
+++ app/components/footer.js
@@ -0,0 +1,5 @@
+export default function FooterPage(){
+    return (
+        <div className="footer">Copyright by Veer @2025</div>
+    )
+}
---
+++ app/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
